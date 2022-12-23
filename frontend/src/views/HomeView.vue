a96b3e7d55afb9c7cc5a3da83606880422fcc3cc
Anirudha Jadhav <anirudhasj441@gmail.com>
2022-12-23 22:37:40 +0530

    dev: update skill section and change primary color from #9379B7 to #11FFEE

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -8,9 +8,11 @@ import TheWelcome from '../components/TheWelcome.vue'
       <div class="row h-100">
         <div class="full-height col-lg-6 col-sm-6 order-md-last text-end">
           <img src="../assets/banner_image.svg" class="" alt="" style="max-width: 100%;height: 100%;">
-          <a href="https://storyset.com/technology" target="_blank" class="attribution">Technology illustrations by Storyset</a>
+          <a href="https://storyset.com/technology" target="_blank" class="attribution">Technology illustrations by
+            Storyset</a>
         </div>
-        <div class="full-height d-flex flex-column justify-content-center animate__animated animate__fadeInLeft col-lg-6 col-sm-6 h-100">
+        <div
+          class="full-height d-flex flex-column justify-content-center animate__animated animate__fadeInLeft col-lg-6 col-sm-6 h-100">
           <p class="m-0" style="font-size: 1rem;">Hello I am, </p>
           <h1 class="title-text" style="color: var(--color-primary);">Anirudha Jadhav</h1>
           <p class="m-0">
@@ -25,27 +27,19 @@ import TheWelcome from '../components/TheWelcome.vue'
         </div>
       </div>
     </section>
-    <section id="skills" class="container p-5">
+    <section id="skills" class="container pt-5">
       <!-- <div style="position: relative;"> -->
-        <h1 class="title-text text-center" style="color: var(--color-primary);font-weight: bold;">Skills</h1>
-        <div class="skill-container row gx-3 p-3 rounded">
-          <div class="col skill rounded">
-            <img src="../assets/logo.svg" alt="">
-            <h4 class="m-0">Vue JS</h4>
-          </div>
-          <div class="col skill rounded">
-            <img src="../assets/logo.svg" alt="">
-            <h4 class="m-0">Vue JS</h4>
-          </div>
-          <div class="col skill rounded">
-            <img src="../assets/logo.svg" alt="">
-            <h4 class="m-0">Vue JS</h4>
-          </div>
-          <div class="col skill rounded">
-            <img src="../assets/logo.svg" alt="">
-            <h4 class="m-0">Vue JS</h4>
+      <h1 class="title-text text-center" style="color: var(--color-primary);font-weight: bold;">Skills</h1>
+      <div class="skill-container mx-auto pb-2 px-1 rounded-3">
+        <div class="row g-2 w-100 m-0">
+          <div v-for="i in 8" key="i" class="col-md-3 col-6 p-0">
+            <div class="skill mx-auto rounded-3 d-flex justify-content-evenly align-items-center">
+              <img src="../assets/logo.svg" alt="">
+              <h6 class="m-0">Vue JS</h6>
+            </div>
           </div>
         </div>
+      </div>
       <!-- </div> -->
     </section>
   </main>
@@ -56,6 +50,7 @@ import TheWelcome from '../components/TheWelcome.vue'
   min-height: calc(100vh - var(--navbar-height)) !important;
 
 }
+
 /* #banner {
   margin-top: var(--navbar-height) !important;
 } */
@@ -85,42 +80,36 @@ import TheWelcome from '../components/TheWelcome.vue'
   font-size: 3.5rem;
   font-family: 'Secular One', sans-serif;
 }
-.skill img{
-  width: 30px;
+
+.skill img {
+  width: 1.5rem;
   /* height: 60%; */
   margin-right: 0.3rem;
 }
-.skill h4{
-  font-family: 'Secular One', sans-serif;;
+
+.skill h4 {
+  font-family: 'Secular One', sans-serif;
+  ;
   /* color: var(--color-primary); */
 }
-.skill{
-  /* height: 100px;
-  width: 100px; */
-  /* width: max-content; */
+
+.skill {
+  width: 95%;
   padding: 0.8rem;
-  margin: 0 0.5rem;
-  background-color: var(--color-background-mute);
+  background-color: var(--color-background);
   display: flex;
-  /* flex-direction: column; */
-  /* justify-content: space-evenly; */
-  /*align-items: center; */
+  color: white;
 }
+
 .skill-container {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  /* mwidth: 90vw !important; */
+  width: 95%;
+  max-width: 600px;
   flex-wrap: wrap;
-  background-color: var(--color-border-hover);
-}
-@media(max-width: 576px) {
-  .skill-container .skill {
-    margin-top: 1rem !important;
-  }
-  /* .skill-container:first-child {
-    margin-top: 0 !;
-  } */
+  background-color: var(--color-primary);
+  /* opacity: 0.5; */
 }
 </style>
