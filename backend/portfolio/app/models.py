db2936dec6178d45f03de93845d397b06034b0c2
Anirudha Jadhav <anirudhasj441@gmail.com>
2023-07-23 14:03:15 +0530

    v1.0.1 add category to skills model

--- backend/portfolio/app/models.py
+++ backend/portfolio/app/models.py
@@ -26,8 +26,15 @@ class Profile(models.Model):
         return str(self.name)
 
 class Skill(models.Model):
+    choices = [
+        ("frontend", "Frontend"),
+        ("backend", "Backend"),
+        ("database", "Database"),
+        ("Other", "Others")
+    ]
     skill = models.CharField(max_length=100, null=True, blank=True)
     icon = models.FileField(upload_to=iconPath, null=True, blank=True)
+    category= models.TextField(choices=choices, null=True, blank=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='skills', null=True, blank=True)
     def __str__(self):
         return str(f"{self.skill} : {self.icon.url if self.icon else None}")
